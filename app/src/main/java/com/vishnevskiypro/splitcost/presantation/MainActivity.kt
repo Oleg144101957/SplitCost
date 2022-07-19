268e70d7683b1556d16f52aa50bcb5d1d545affb
Oleg144101957 <vishnevskiyoleg@gmail.com>
2022-07-19 17:38:06 +0300

    second commit

--- app/src/main/java/com/vishnevskiypro/splitcost/presantation/MainActivity.kt
+++ app/src/main/java/com/vishnevskiypro/splitcost/presantation/MainActivity.kt
@@ -1,12 +1,14 @@
-package com.vishnevskiypro.splitcost
+package com.vishnevskiypro.splitcost.presantation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.vishnevskiypro.splitcost.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
 
         //Let's code
 
---
+++ app/src/main/java/com/vishnevskiypro/splitcost/presantation/СostScreen.kt
@@ -0,0 +1,14 @@
+package com.vishnevskiypro.splitcost.presantation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class CostScreen : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+
+}
